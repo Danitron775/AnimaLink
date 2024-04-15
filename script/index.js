6755ab96d8af8031a80de3cf4b9281586f09c3d6
Daniel Ruiz <danielfelipe1877@gmail.com>
2024-04-15 12:33:11 -0500

    Actualización de botones y contenido principal en inicio.html y efecto scroll en index.js

---
+++ script/index.js
@@ -0,0 +1,10 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    console.log(document.body.scrollTop);
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("header-navbar").classList.add("scrolled");
+  } else {
+    document.getElementById("header-navbar").classList.remove("scrolled");
+  }
+}
