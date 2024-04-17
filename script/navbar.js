3213e793a1d15ddf9d807245b1439cc7875bfc6f
Daniel Ruiz <danielfelipe1877@gmail.com>
2024-04-17 13:50:06 -0500

    Creación de animales-necesitados.html, optimización de estilos en navbar.css, cambio de clases automatizado en index.js sobree navbar.html

--- script/navbar.js
+++ script/navbar.js
@@ -6,7 +6,6 @@ $(document).ready(function () {
 window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-  console.log(document.body.scrollTop);
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("header-navbar").classList.add("scrolled");
   } else {
