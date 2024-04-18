f6b062a2efb89e145857d4a374eb57a14c01ad83
Daniel Ruiz <danielfelipe1877@gmail.com>
2024-04-18 00:33:25 -0500

    Actualización de links en footer.html, styles/inicio.css, y navbar.html, y creación de función responsive en script/index.js y styles/styles.css

--- script/index.js
+++ script/index.js
@@ -1,8 +1,29 @@
 // FUNCIÓM PARA CARGAR EL NAVBAR Y FOOTER EN TODAS LAS PÁGINAS
 $(document).ready(function () {
     $('.navbarContainer').load('navbar.html');
-  });
+});
 
 $(document).ready(function () {
     $('.footerContainer').load('footer.html');
-  });
+});
+
+// FUNCIÓN PARA CAPTURAR EL TAMAÑO DE LA PANTALLA PARA MANEJO DE INTERFAZ RESPONSIVA
+$(document).ready(function () {
+    // Definir la función captureScreenSize
+    function captureScreenSize() {
+        var screenWidth = $(window).width();
+        var animals = $('#anim-necesitados-primera-parte');
+
+        if (screenWidth < 992) {
+            animals.removeClass('style-light');
+        } else {
+            animals.addClass('style-light');
+        }
+    }
+
+    // LLAAMAR A LA FUNCIÓN AL CARGAR LA PÁGINA
+    captureScreenSize();
+
+    // LLAAMAR A LA FUNCIÓN AL CAMBIAR EL TAMAÑO DE LA PANTALLA
+    $(window).resize(captureScreenSize);
+});
