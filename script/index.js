ac1aa4aa7abd84bafa0d1859be66a6a99160672f
Daniel Ruiz <danielfelipe1877@gmail.com>
2024-04-17 20:01:16 -0500

    Migración de estilos generales a styles.css, creación de tercer y cuarta parte en la pagina inicio.html, creación de footer reutilizable, optimización y migración de funciones de la navbar en navbar.js

--- script/index.js
+++ script/index.js
@@ -1,42 +1,8 @@
+// FUNCIÓM PARA CARGAR EL NAVBAR Y FOOTER EN TODAS LAS PÁGINAS
 $(document).ready(function () {
-    var url = window.location.pathname;
-    let inicio = document.getElementById("inicio");
-    let animimals = document.getElementById("anim-necesitados");
-    let apadrina = document.getElementById("apadrinamiento");
-    let contacto = document.getElementById("contacto");
-    let faq = document.getElementById("faq");
+    $('.navbarContainer').load('navbar.html');
+  });
 
-    if (url.includes("inicio.html")) {
-        inicio.classList.add("active");
-        animimals.classList.add("inactive");
-        apadrina.classList.add("inactive");
-        contacto.classList.add("inactive");
-        faq.classList.add("inactive");
-    } else if (url.includes("animales-necesitados.html")) {
-        animimals.classList.add("active");
-        inicio.classList.add("inactive");
-        apadrina.classList.add("inactive");
-        contacto.classList.add("inactive");
-        faq.classList.add("inactive");
-    } else if (url.includes("apadrinamiento.html")) {
-        apadrina.classList.add("active");
-        inicio.classList.add("inactive");
-        animimals.classList.add("inactive");
-        contacto.classList.add("inactive");
-        faq.classList.add("inactive");
-    } else if (url.includes("contacto.html")) {
-        contacto.classList.add("active");
-        inicio.classList.add("inactive");
-        animimals.classList.add("inactive");
-        apadrina.classList.add("inactive");
-        faq.classList.add("inactive");
-    } else if (url.includes("faq.html")) {
-        faq.classList.add("active");
-        inicio.classList.add("inactive");
-        animimals.classList.add("inactive");
-        apadrina.classList.add("inactive");
-        contacto.classList.add("inactive");
-    }
-
-
-});
+$(document).ready(function () {
+    $('.footerContainer').load('footer.html');
+  });
--- script/navbar.js
+++ script/navbar.js
@@ -1,8 +1,27 @@
 $(document).ready(function () {
-  $('.navbarContainer').load('navbar.html');
+  var url = window.location.pathname;
+
+  // OBJETO QUE MAPEREA LAS CLAVES DE LOS ELEMENTOS DEL NAVBAR CON SUS VALORES
+  var navItems = {
+    inicio: document.getElementById("inicio"),
+    'animales-necesitados': document.getElementById("anim-necesitados"),
+    apadrinamiento: document.getElementById("apadrinamiento"),
+    contacto: document.getElementById("contacto"),
+    faq: document.getElementById("faq")
+  };
+
+  // ITERAR SOBRE EL OBJETO navItems Y AÑADIR LA CLASE "active" AL ELEMENTO DEL NAVBAR QUE CORRESPONDA
+  for (var key in navItems) {
+    if (url.includes(key + ".html")) {
+      navItems[key].classList.add("active");
+    } else {
+      navItems[key].classList.add("inactive");
+    }
+  }
 });
 
 
+// FUNCIÓN PARA UNA NAVBAR STICKY
 window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
