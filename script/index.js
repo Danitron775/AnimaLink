037ad47d1b9b6b52ad93d52d4711d0492fce3ed9
Daniel Ruiz <danielfelipe1877@gmail.com>
2024-04-18 22:01:12 -0500

    Actualización en card.css y navbar.css, y refactorización en index.js para diseño responsive

--- script/index.js
+++ script/index.js
@@ -1,29 +1,30 @@
-// FUNCIÓM PARA CARGAR EL NAVBAR Y FOOTER EN TODAS LAS PÁGINAS
-$(document).ready(function () {
-    $('.navbarContainer').load('navbar.html');
-});
+function captureScreenSize() {
+    let screenWidth = $(window).width();
+    let animals = $('#anim-necesitados-primera-parte');
 
-$(document).ready(function () {
-    $('.footerContainer').load('footer.html');
-});
+    if (screenWidth < 992) {
+        animals.removeClass('style-light');
+    } else {
+        animals.addClass('style-light');
+    }
+}
 
 // FUNCIÓN PARA CAPTURAR EL TAMAÑO DE LA PANTALLA PARA MANEJO DE INTERFAZ RESPONSIVA
 $(document).ready(function () {
     // Definir la función captureScreenSize
-    function captureScreenSize() {
-        var screenWidth = $(window).width();
-        var animals = $('#anim-necesitados-primera-parte');
 
-        if (screenWidth < 992) {
-            animals.removeClass('style-light');
-        } else {
-            animals.addClass('style-light');
-        }
-    }
 
     // LLAAMAR A LA FUNCIÓN AL CARGAR LA PÁGINA
     captureScreenSize();
 
     // LLAAMAR A LA FUNCIÓN AL CAMBIAR EL TAMAÑO DE LA PANTALLA
     $(window).resize(captureScreenSize);
+});
+
+
+
+// FUNCIÓM PARA CARGAR EL NAVBAR Y FOOTER EN TODAS LAS PÁGINAS
+$(document).ready(function () {
+    $('.navbarContainer').load('navbar.html');
+    $('.footerContainer').load('footer.html');
 });
